ed20b0e41a8b3403cf9770b0a940cc2a77c6417e
Abhishek Kumar <153679537+Abhishek-Kumar-Ghub@users.noreply.github.com>
2026-01-08 01:14:49 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 doenv.config();
 const connectionDb = async () => {
 
+    
     try {
         await mongoose.connect(process.env.MONGODB_URL);
         console.log('database is connected successfully');
