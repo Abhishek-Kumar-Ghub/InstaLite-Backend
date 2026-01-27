aa4a621a91b236c460bb722bb8007958e9ff5286
Abhishek Kumar <153679537+Abhishek-Kumar-Ghub@users.noreply.github.com>
2026-01-27 15:40:09 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,16 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+doenv.config();
+
+
+const connectionDb = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log('database is connected successfully');
+    } catch (error) {
+        console.error('connection failed', error);
+    }
+};
+export default connectionDb;
 
