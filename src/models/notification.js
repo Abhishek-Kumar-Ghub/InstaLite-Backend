9403f241d6eea97ccade9d862f6116f07ee95446
Abhishek Kumar <153679537+Abhishek-Kumar-Ghub@users.noreply.github.com>
2026-04-25 23:08:43 +0530

    Update notification.js

--- src/models/notification.js
+++ src/models/notification.js
@@ -6,6 +6,7 @@ const notificationSchema = new mongoose.Schema({
         ref:"User",
         required:true,
     },
+    //sender
     sender:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
