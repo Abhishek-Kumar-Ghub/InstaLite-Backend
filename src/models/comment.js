bad5d4338e2bf72eb3faf5be7e14b2258d1d5dc8
Abhishek Kumar <153679537+Abhishek-Kumar-Ghub@users.noreply.github.com>
2026-04-29 23:18:43 +0530

    Update comment.js

--- src/models/comment.js
+++ src/models/comment.js
@@ -5,6 +5,7 @@ const commentSchema= new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"Post",
     },
+    //author
     author:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
