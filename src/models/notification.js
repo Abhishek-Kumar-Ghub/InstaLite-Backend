2e249232283e22b03f8b4279a660b83fd5aa1b65
Abhishek Kumar <153679537+Abhishek-Kumar-Ghub@users.noreply.github.com>
2026-05-03 00:06:17 +0530

    Update notification.js

--- src/models/notification.js
+++ src/models/notification.js
@@ -21,7 +21,8 @@ const notificationSchema = new mongoose.Schema({
     post:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"Post",
-        },
+        }
+        //isRead
     isRead:{
         type:Boolean,
         default:false,
